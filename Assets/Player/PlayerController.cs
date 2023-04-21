63b737b34d78dde22274d4197cc37188289ceeb8
Jorge Veiga Jr <jorgeveiga0@gmail.com>
2023-04-21 16:05:37 -0400

    Added player interaction with NPC, added Yarn Spinner for dialogues, added UI assets.

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed = 1;
     
     private Vector2 _movementInput;
+    private bool nearNpc;
+    private bool talking;
+    private NpcBehavior npc;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +23,43 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _rigidbody.MovePosition(_rigidbody.position + (_movementInput * Time.fixedDeltaTime * moveSpeed));
+        if (!talking)
+            _rigidbody.MovePosition(_rigidbody.position + (_movementInput * Time.fixedDeltaTime * moveSpeed));
     }
 
     private void OnMove(InputValue inputValue)
     {
         _movementInput = inputValue.Get<Vector2>();
+    }
+
+    private void OnInteract(InputValue inputValue)
+    {
+        if (npc)
+        {
+            if (!talking)
+            {
+                talking = true;
+                npc.StartDialogue();
+                npc.onDialogueComplete.AddListener(OnDialogueComplete);
+            }
+        }
+    }
+
+    private void OnDialogueComplete()
+    {
+        talking = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        nearNpc = other.CompareTag("Npc");
+
+        if (nearNpc)
+            npc = other.GetComponent<NpcBehavior>();
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        npc = null;
     }
 }
